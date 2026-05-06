b2a053e6105ba3b8f3b7bb0baf63c3429592a129
AbidMahid <abidmahid2015@gmail.com>
2026-05-07 00:11:47 +0600

    SimpleCalculatorIfElse

---
+++ 3_SimpleCalculatorIfElse/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char op;
+    double num1, num2;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    if (op == '+')
+        cout << "Result = " << num1 + num2 << endl;
+    else if (op == '-')
+        cout << "Result = " << num1 - num2 << endl;
+    else if (op == '*')
+        cout << "Result = " << num1 * num2 << endl;
+    else if (op == '/')
+        cout << "Result = " << num1 / num2 << endl;
+    else
+        cout << "Invalid operator!" << endl;
+
+    return 0;
+}
