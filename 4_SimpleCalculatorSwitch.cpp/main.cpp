d464509eaa33f91b4391e8c53d0dcd4299ccc635
AbidMahid <abidmahid2015@gmail.com>
2026-05-08 01:12:33 +0600

    SimpleCalculator

---
+++ 4_SimpleCalculatorSwitch.cpp/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char op;
+    double num1, num2, result;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result = " << result << endl;
+            break;
+
+        case '-':
+            result = num1 - num2;
+            cout << "Result = " << result << endl;
+            break;
+
+        case '*':
+            result = num1 * num2;
+            cout << "Result = " << result << endl;
+            break;
+
+        case '/':
+            if (num2 != 0)
+                cout << "Result = " << num1 / num2 << endl;
+            else
+                cout << "Error: Division by zero!" << endl;
+            break;
+
+        default:
+            cout << "Invalid operator!" << endl;
+    }
+
+    return 0;
+}
