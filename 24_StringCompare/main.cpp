6c28bc45969e0211802d2838b3cbb07cda2e431e
AbidMahid <abidmahid2015@gmail.com>
2026-05-19 23:25:34 +0600

    String Comparision

---
+++ 24_StringCompare/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a = "hello";
+    string b = "hello";
+
+    if (a == b)
+        cout << "Strings are equal";
+    else
+        cout << "Not equal";
+
+    return 0;
+}
