d97127d009f1f4e221fc675bd8c1de88bce88fad
AbidMahid <abidmahid2015@gmail.com>
2026-05-22 00:07:36 +0600

    Class Constructor

---
+++ 28_ClassConstructor/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+public:
+    string name;
+
+    Student() {
+        name = "Default";
+    }
+};
+
+int main() {
+    Student s;
+    cout << s.name;
+
+    return 0;
+}
