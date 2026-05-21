d65734477402b67945ccad0e2833979a34fda8ab
AbidMahid <abidmahid2015@gmail.com>
2026-05-22 00:06:55 +0600

    Student Input

---
+++ 27_ClassStudentInput/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+public:
+    string name;
+    int age;
+
+    void input() {
+        cin >> name >> age;
+    }
+
+    void display() {
+        cout << name << " " << age << endl;
+    }
+};
+
+int main() {
+    Student s;
+    s.input();
+    s.display();
+
+    return 0;
+}
